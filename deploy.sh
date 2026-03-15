0fe509d99fa1856d641c421eb1f0eea378ce8319
Leon Kiong <leonkiongwei@gmail.com>
2026-03-15 16:36:29 +0800

    updated deploy.sh to remove existing files in staging folde before copying files over.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-scp -r ./* leon@165.22.53.96:/home/leon/site-upload/
+ssh leon@165.22.53.96 "rm -rf /home/leon/site-upload/*"
+scp *.html leon@165.22.53.96:/home/leon/site-upload/
+scp -r assets leon@165.22.53.96:/home/leon/site-upload/
 ssh leon@165.22.53.96 "~/deploy-static-site.sh"
 echo "Remote deploy finished."
